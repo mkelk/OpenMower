a6b54403cc7028a12a50389f13f39ec072b32604
mkelk <morten@elk.dk>
2022-05-16 00:50:04 +0200

    add to set-up

--- configs/melk_extra_setup/melk_openmower_setup.sh
+++ configs/melk_extra_setup/melk_openmower_setup.sh
@@ -4,15 +4,27 @@
 sudo apt remove unattended-upgrades
 
 # get latest sources and updates
-sudo apt update
-sudo apt upgrade
+sudo apt update 
+sudo apt upgrade -y
 
 # getting ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt update
-sudo apt install ros-noetic-ros-base
+sudo apt update 
+sudo apt install ros-noetic-ros-base -y
 
+# setting up OpenMower
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
+cd ~/OpenMower/ROS/
+rosdep install --from-paths src --ignore-src -y
+
+# compiling 
+catkin_make
+catkin_make
+catkin_make
+catkin_make
 
 
 cd ~/OpenMower/configs/melk_extra_setup
